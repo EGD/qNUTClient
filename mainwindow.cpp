3ed0b6f15c3c0fa339b7c9df4655f67ea53a92bd
Andrey Karepin <egdfree@opensuse.org>
2011-05-16 14:48:43 +0400

    Connect work!

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,7 +6,9 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    client = new upsClient("localhost",3493);
+    client = new upsClient("localhost", 3493);
+    QObject::connect(client->ASocket, SIGNAL(connected()), this, SLOT(slotConnected()));
+    QObject::connect(client->ASocket, SIGNAL(readyRead()), this, SLOT(slotReadyRead()));
 }
 
 MainWindow::~MainWindow()
@@ -17,35 +19,56 @@ MainWindow::~MainWindow()
 
 void MainWindow::slotConnected()
 {
-    textLines->append("CONNECTED");
+    qDebug() << "CONNECTED";
+//Autohorize
+    client->ASocket->write(QString("USERNAME %1\n").arg("upsmaster").toAscii());
+    client->ASocket->write(QString("PASSWORD %1\n").arg("fcd3e41583").toAscii());
+    client->ASocket->write(QString("LOGIN %1\n").arg("myups").toAscii());
+    client->ASocket->waitForBytesWritten();
+    qDebug() << "AUTH SEND";
 }
 
 void MainWindow::slotReadyRead()
 {
+    qDebug() << "READY_READ";
     QTextStream in(client->ASocket);
-    QString str;
-    in >> str;
-    textLines->append(str);
+    ui->textLines->append(in.readAll());
 }
 
 upsClient::upsClient(const QString &host, const int port)
 {
     ASocket = new QTcpSocket(this);
 
-    QObject::connect(ASocket, SIGNAL(connected()), this, SLOT(slotDoConnected()));
-    QObject::connect(ASocket, SIGNAL(readyRead()), this, SLOT(slotReadyRead()));
     QObject::connect(ASocket, SIGNAL(error(QAbstractSocket::SocketError)),
                      this, SLOT(slotError(QAbstractSocket::SocketError)));
 
-    ASocket->connectToHost(host, port, QIODevice::ReadOnly/*Write*/);
+    ASocket->connectToHost(host, port, QIODevice::ReadWrite);
 }
 
 upsClient::~upsClient()
 {
+    ASocket->disconnectFromHost();
+    qDebug() << "DISCONNECTED";
     delete ASocket;
 }
 
 void upsClient::slotError(QAbstractSocket::SocketError err)
 {
+    QString errStr;
+    switch (err) {
+    case QAbstractSocket::HostNotFoundError:
+        errStr = "The host was not found.";
+        break;
+    case QAbstractSocket::RemoteHostClosedError:
+        errStr = "The remote host is closed";
+        break;
+    case QAbstractSocket::ConnectionRefusedError:
+        errStr = "The connection was refused.";
+        break;
+    default:
+        errStr = ASocket->errorString();
+        break;
+    }
 
+    QMessageBox::critical(0,"Error",errStr);
 }
